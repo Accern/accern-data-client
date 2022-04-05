570c820bd47b0a3f02c2771dbb4721641257883d
TheAnkurGoswami <kinglusifer1401@gmail.com>
2022-04-05 19:19:03 +0530

    dynamic naming

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -9,13 +9,14 @@ from accern_data.util import load_json
 def test_csv_full_csv_date_consistency() -> None:
     start_date = "2022-01-03"
     end_date = "2022-03-04"
+    output_pattern = "test-data-2022"
     client = accern_data.create_data_client(
         "http://api.example.com/", "SomeRandomToken")
     client.set_mode("csv_date")
     client.download_range(
         start_date=start_date,
         output_path="./tests/outputs/",
-        output_pattern="test-data-2022",
+        output_pattern=output_pattern,
         end_date=end_date,
         verbose=True)
     csv_date_arr = []
@@ -23,7 +24,7 @@ def test_csv_full_csv_date_consistency() -> None:
         date = cur_date.strftime("%Y-%m-%d")
         try:
             df_generated = pd.read_csv(
-                f"tests/outputs/test-data-2022-{date}.csv")
+                f"tests/outputs/{output_pattern}-{date}.csv")
             csv_date_arr.append(df_generated)
         except FileNotFoundError:
             continue
@@ -33,11 +34,11 @@ def test_csv_full_csv_date_consistency() -> None:
     client.download_range(
         start_date=start_date,
         output_path="./tests/outputs/",
-        output_pattern="test-data-2022",
+        output_pattern=output_pattern,
         end_date=end_date,
         verbose=True)
     combined_df.to_csv("combined.csv")
-    df_csv_full = pd.read_csv("tests/outputs/test-data-2022.csv")
+    df_csv_full = pd.read_csv(f"tests/outputs/{output_pattern}.csv")
 
     pd_test.assert_frame_equal(
         df_csv_full[sorted(df_csv_full.columns)],
@@ -47,13 +48,14 @@ def test_csv_full_csv_date_consistency() -> None:
 def test_json_csv_date_consistency() -> None:
     start_date = "2022-01-03"
     end_date = "2022-03-04"
+    output_pattern = "test-data-2022"
     client = accern_data.create_data_client(
         "http://api.example.com/", "SomeRandomToken")
     client.set_mode("csv_date")
     client.download_range(
         start_date=start_date,
         output_path="./tests/outputs/",
-        output_pattern="test-data-2022",
+        output_pattern=output_pattern,
         end_date=end_date,
         verbose=True)
 
@@ -61,15 +63,14 @@ def test_json_csv_date_consistency() -> None:
     client.download_range(
         start_date=start_date,
         output_path="./tests/outputs/",
-        output_pattern="test-data-2022",
+        output_pattern=output_pattern,
         end_date=end_date,
         verbose=True)
-
     for cur_date in pd.date_range(start_date, end_date):
         date = cur_date.strftime("%Y-%m-%d")
         try:
-            json_obj = load_json(f"tests/outputs/test-data-2022-{date}.json")
-            csv_obj = pd.read_csv(f"tests/outputs/test-data-2022-{date}.csv")
+            json_obj = load_json(f"tests/outputs/{output_pattern}-{date}.json")
+            csv_obj = pd.read_csv(f"tests/outputs/{output_pattern}-{date}.csv")
         except FileNotFoundError:
             continue
         csv_obj["event_accern_id"] = csv_obj["event_accern_id"].astype("str")
@@ -80,10 +81,11 @@ def test_json_csv_date_consistency() -> None:
             "event_hits",
             "entity_hits",
             "entity_indices",
-            "event_text",
             "entity_text",
         }
         for col in jsonified_cols:
             csv_obj[col] = csv_obj[col].apply(json.loads)
+        csv_obj["entity_sector"].fillna("N/A", inplace=True)
+        csv_obj.fillna("", inplace=True)
         csv_json = csv_obj.to_dict("records")
         assert csv_json == json_obj, f"Results for {date} are different."
