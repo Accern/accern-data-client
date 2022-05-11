de4dc164483156b6e0e783ead1479e6c12acb99a
TheAnkurGoswami <kinglusifer1401@gmail.com>
2022-05-11 21:11:16 +0530

    coverage

---
+++ coverage/coverage.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+coverage erase
+
+find * \( -name '*.py' -o -name '*.pyi' \) -and -not -path './venv/*' \
+ -and -not -path './stubs/*' -exec python3 -m compileall -q -j 0 {} +
+
+
+run_coverage() {
+    python -m pytest -xvv $1 --cov --cov-append
+}
+export -f run_coverage
+
+for CUR in $(find 'tests' \( -name '*.py' -and -name 'test_*' \) \
+            -and -not -path 'tests/data/*' \
+            -and -not -path 'tests/__pycache__/*' |
+            sort -sf); do
+    run_coverage ${CUR}
+done
+
+coverage report
+coverage html
+coverage erase
