70410b02526e746101ddfaa92dd13d5c9c20e662
TheAnkurGoswami <kinglusifer1401@gmail.com>
2022-07-28 23:11:22 +0530

    Update test_timestamps.py

--- tests/test_timestamps.py
+++ tests/test_timestamps.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import pandas.testing as pd_test
 from accern_data import create_data_client, DATE_FORMAT
-from accern_data.util import DATA_DIR, EXAMPLE_URL, load_json
+from accern_data.util import (
+    DATA_DIR,
+    EXAMPLE_URL,
+    get_data_dir,
+    load_json,
+    set_data_dir,
+)
 
 OUTPUT_PATH = "tests/outputs/"
 
 
 def test_timestamp_csv_date() -> None:
-    start_date = "2022-01-06T03:19:15"
-    end_date = "2022-02-02T08:27:50"
+    set_data_dir("tests/data_mini")
+    start_date = "2022-01-07T05:04:11"
+    end_date = "2022-02-03T03:28:09"
     output_path = OUTPUT_PATH
     output_pattern = "test_timestamp_csv_date"
     client = create_data_client(EXAMPLE_URL, "SomeRandomToken")
@@ -25,7 +32,7 @@ def test_timestamp_csv_date() -> None:
     for cur_date in pd.date_range(start_date, end_date):
         date = cur_date.strftime(DATE_FORMAT)
         try:
-            df_actual = pd.read_csv(f"{DATA_DIR}/csv_date/{date}.csv")
+            df_actual = pd.read_csv(f"{get_data_dir()}/csv_date/{date}.csv")
             df_generated = pd.read_csv(
                 f"{output_path}{output_pattern}-{date}.csv")
         except FileNotFoundError:
@@ -42,8 +49,9 @@ def test_timestamp_csv_date() -> None:
 
 
 def test_timestamp_csv_full() -> None:
-    start_date = "2022-01-06T03:19:15"
-    end_date = "2022-02-02T08:27:50"
+    set_data_dir("tests/data_mini")
+    start_date = "2022-01-07T05:04:11"
+    end_date = "2022-02-03T03:28:09"
     output_path = OUTPUT_PATH
     output_pattern = "test_timestamp_csv_full"
     client = create_data_client(EXAMPLE_URL, "SomeRandomToken")
@@ -56,7 +64,7 @@ def test_timestamp_csv_full() -> None:
         mode=("csv", False),
         indicator="message")
 
-    df_actual = pd.read_csv(f"{DATA_DIR}/data-2022.csv")
+    df_actual = pd.read_csv(f"{get_data_dir()}/data-2022.csv")
     df_generated = pd.read_csv(f"{output_path}{output_pattern}.csv")
 
     start_date_dt = pd.to_datetime(start_date, utc=True)
@@ -77,8 +85,9 @@ def test_timestamp_csv_full() -> None:
 
 
 def test_timestamp_json() -> None:
-    start_date = "2022-01-06T03:19:15"
-    end_date = "2022-02-02T08:27:50"
+    set_data_dir("tests/data_mini")
+    start_date = "2022-01-07T05:04:11"
+    end_date = "2022-02-03T03:28:09"
     output_path = "tests/outputs/"
     output_pattern = "test_timestamp_json"
     client = create_data_client(EXAMPLE_URL, "SomeRandomToken")
@@ -95,7 +104,7 @@ def test_timestamp_json() -> None:
     for cur_date in pd.date_range(start_date, end_date):
         date = cur_date.strftime(DATE_FORMAT)
         try:
-            json_actual = load_json(f"{DATA_DIR}/json/{date}.json")
+            json_actual = load_json(f"{get_data_dir()}/json/{date}.json")
             json_generated = load_json(
                 f"{output_path}{output_pattern}-{date}.json")
         except FileNotFoundError:
